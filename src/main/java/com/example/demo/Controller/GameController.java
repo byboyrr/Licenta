2c1c01915e103050f0c9ddae5a2492e7136d1071
byboyrr <rarespausan@yahoo.com>
2020-08-27 17:08:50 +0300

    v1.0.2 cu login cu compareTO + gresit->default + adaugare in baza de date meciuri+ v.2.0+ lista meciuri jucate + lista meciuri care se joaca+ cote de tip double+ remove CotaGame

--- src/main/java/com/example/demo/Controller/GameController.java
+++ src/main/java/com/example/demo/Controller/GameController.java
@@ -47,7 +47,7 @@ public class GameController {
         return gameService.saveGame(game);
     }
 
-    @GetMapping("/laba")
+    @GetMapping("/add-data")
     public String preluareDate(){
         String line = "";
         String rezultat="";
@@ -57,7 +57,7 @@ public class GameController {
 //parsing a CSV file into BufferedReader class constructor
             int nr =0;
 
-            BufferedReader br = new BufferedReader(new FileReader("src/main/resources/spania2.csv"));
+            BufferedReader br = new BufferedReader(new FileReader("src/main/resources/italia.csv"));
             while ((line = br.readLine()) != null)   //returns a Boolean value
             {
                 String[] meci= line.split(splitBy);    // use comma as separator
